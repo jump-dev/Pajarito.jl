4422a55fd5a564aa535680c50161a7ae6ba77e91
Emre Yamangil <emreyamangil@gmail.com>
2016-03-14 22:45:34 -0400

    initial license

--- src/conic.jl
+++ src/conic.jl
@@ -1,3 +1,7 @@
+#  Copyright 2016, Los Alamos National Laboratory, LANS LLC.
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #=========================================================
  This solver implements mixed-integer conic programming
  algorithm found in:
--- test/nlptest.jl
+++ test/nlptest.jl
@@ -1,3 +1,8 @@
+#  Copyright 2016, Los Alamos National Laboratory, LANS LLC.
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 function runnonlineartests(algorithm, mip_solvers)
 
 facts("Convex constraint with LB and UB") do
