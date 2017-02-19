82fe5e90637ce9529b8047aeccbcc69028e9f54c
Chris C <coey.chris@gmail.com>
2017-02-19 18:35:40 -0500

    debug cuts

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,12 +40,12 @@ if eco
     push!(solvers_soc, ECOS.ECOSSolver(verbose=false))
     push!(solvers_expsoc, ECOS.ECOSSolver(verbose=false))
 end
-# if scs
-#     push!(solvers_soc, SCS.SCSSolver(eps=1e-5,max_iters=100000,verbose=0))
-#     push!(solvers_expsoc, SCS.SCSSolver(eps=1e-5,max_iters=100000,verbose=0))
-#     push!(solvers_sdpsoc, SCS.SCSSolver(eps=1e-6,max_iters=1000000,verbose=0))
-#     push!(solvers_sdpexp, SCS.SCSSolver(eps=1e-6,max_iters=1000000,verbose=0))
-# end
+if scs
+    push!(solvers_soc, SCS.SCSSolver(eps=1e-6,max_iters=100000,verbose=0))
+    push!(solvers_expsoc, SCS.SCSSolver(eps=1e-6,max_iters=100000,verbose=0))
+    push!(solvers_sdpsoc, SCS.SCSSolver(eps=1e-7,max_iters=1000000,verbose=0))
+    push!(solvers_sdpexp, SCS.SCSSolver(eps=1e-7,max_iters=1000000,verbose=0))
+end
 if mos
     push!(solvers_soc, Mosek.MosekSolver(LOG=0))
     push!(solvers_sdpsoc, Mosek.MosekSolver(LOG=0))
