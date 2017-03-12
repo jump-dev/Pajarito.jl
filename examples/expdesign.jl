5b02e1f9385832dad11e1fade61241d8a3710f09
Chris Coey <coey.chris@gmail.com>
2017-03-12 18:35:44 -0400

    clean up runtests, change expdesign problems to have V with q rank

--- examples/expdesign.jl
+++ examples/expdesign.jl
@@ -8,7 +8,6 @@ using Convex, JuMP, Pajarito
 # q is dimension of estimate space
 # p is number of experimental vectors
 function gen_V(q, p)
-    srand(100)
     V = Array{Float64}(q, p)
     for i in 1:q, j in 1:p
         v = randn()
@@ -149,7 +148,7 @@ end
 #=========================================================
 Choose solvers and options
 =========================================================#
-mip_solver_drives = true
+mip_solver_drives = false
 log_level = 3
 rel_gap = 1e-5
 
@@ -159,19 +158,19 @@ rel_gap = 1e-5
 
 using CPLEX
 mip_solver = CplexSolver(
-    # CPX_PARAM_SCRIND=(mip_solver_drives ? 1 : 0),
-    CPX_PARAM_SCRIND=1,
+    CPX_PARAM_SCRIND=(mip_solver_drives ? 1 : 0),
+    # CPX_PARAM_SCRIND=1,
     CPX_PARAM_EPINT=1e-8,
     CPX_PARAM_EPRHS=1e-7,
     CPX_PARAM_EPGAP=(mip_solver_drives ? 1e-5 : 1e-9)
 )
 
 
-# using SCS
-# cont_solver = SCSSolver(eps=1e-5, max_iters=1000000, verbose=0)
+using SCS
+cont_solver = SCSSolver(eps=1e-6, max_iters=1000000, verbose=0)
 
-using Mosek
-cont_solver = MosekSolver(LOG=0)
+# using Mosek
+# cont_solver = MosekSolver(LOG=0)
 
 
 solver = PajaritoSolver(
@@ -194,14 +193,36 @@ Specify data dimensions
 =========================================================#
 # Uncomment only the line with the desired data dimensions
 (q, p, n, nmax) = (
-    # 100, 250, 500, 5   # Large
-    # 25, 75, 125, 5     # Medium
-    5, 15, 25, 5       # Small
+    # 100, 250, 500, 5   # Huge
+    # 25, 75, 125, 5     # Large
+    # 10, 30, 50, 5      # Medium
+    # 5, 15, 25, 5       # Small
+    4, 8, 12, 3       # Tiny
 )
+@assert (p > q) && (n > q)
 
 # Generate matrix of experimental vectors
-V = gen_V(q, p)
+V = Array{Float64}(q, p)
+srand(100)
 
+tries = 0
+while true
+    # Generate random V
+    V = gen_V(q, p)
+    tries += 1
+
+    # Ensure rank is q
+    if rank(V) == q
+        break
+    end
+
+    if tries > 100
+        error("Could not generate random V matrix with rank equal to q in 100 tries\n")
+    end
+end
+
+@show tries
+@show V
 
 #=========================================================
 Solve Convex.jl models
