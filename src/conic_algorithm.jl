310f3c762411ca962e04cd3aaab462b69975fde2
Miles Lubin <miles.lubin@gmail.com>
2017-03-20 18:04:53 -0400

    update copyright, closes #281

--- src/conic_algorithm.jl
+++ src/conic_algorithm.jl
@@ -1,4 +1,5 @@
-#  Copyright 2016, Los Alamos National Laboratory, LANS LLC, and Chris Coey.
+#  Copyright 2017, Chris Coey and Miles Lubin
+#  Copyright 2016, Los Alamos National Laboratory, LANS LLC.
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, you can obtain one at http://mozilla.org/MPL/2.0/.
--- test/conictest.jl
+++ test/conictest.jl
@@ -1,3 +1,4 @@
+#  Copyright 2017, Chris Coey and Miles Lubin
 #  Copyright 2016, Los Alamos National Laboratory, LANS LLC.
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
