eaefb38a829b4e3f1340a864b37f5580d0d58368
Chris Coey <coey.chris@gmail.com>
2017-03-23 20:48:02 -0400

    enable glpk, ecos

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,12 +40,12 @@ if cpx
         solvers["MISOCP"]["Pajarito(CPLEX, MOSEK)"] = PajaritoSolver(mip_solver=CPLEX.CplexSolver(CPX_PARAM_SCRIND=0, CPX_PARAM_EPINT=1e-8, CPX_PARAM_EPRHS=1e-8, CPX_PARAM_EPGAP=1e-10), cont_solver=Mosek.MosekSolver(LOG=0), log_level=0, rel_gap=1e-6)
     end
 end
-# if glp
-#     solvers["MILP"]["GLPK"] = GLPKMathProgInterface.GLPKSolverMIP(msg_lev=GLPK.MSG_OFF, tol_int=tol_int, tol_bnd=tol_feas, tol_obj=tol_gap)
-#     if eco
-#         solvers["MISOCP"]["Pajarito(GLPK, ECOS)"] = PajaritoSolver(mip_solver=GLPKMathProgInterface.GLPKSolverMIP(presolve=true, msg_lev=GLPK.MSG_OFF, tol_int=1e-8, tol_bnd=1e-8, tol_obj=1e-10), cont_solver=ECOS.ECOSSolver(verbose=false), log_level=0, rel_gap=1e-6)
-#     end
-# end
+if glp
+    solvers["MILP"]["GLPK"] = GLPKMathProgInterface.GLPKSolverMIP(msg_lev=GLPK.MSG_OFF, tol_int=tol_int, tol_bnd=tol_feas, tol_obj=tol_gap)
+    if eco
+        solvers["MISOCP"]["Pajarito(GLPK, ECOS)"] = PajaritoSolver(mip_solver=GLPKMathProgInterface.GLPKSolverMIP(presolve=true, msg_lev=GLPK.MSG_OFF, tol_int=1e-8, tol_bnd=1e-8, tol_obj=1e-10), cont_solver=ECOS.ECOSSolver(verbose=false), log_level=0, rel_gap=1e-6)
+    end
+end
 # if try_import(:SCIP)
 #     solvers["MILP"]["SCIP"] = solvers["MISOCP"]["SCIP"] = SCIP.SCIPSolver("display/verblevel", 0, "limits/gap", tol_gap, "numerics/feastol", tol_feas)
 # end
@@ -64,9 +64,9 @@ solvers["SOC"] = Dict{String,MathProgBase.AbstractMathProgSolver}()
 solvers["Exp+SOC"] = Dict{String,MathProgBase.AbstractMathProgSolver}()
 solvers["PSD+SOC"] = Dict{String,MathProgBase.AbstractMathProgSolver}()
 solvers["PSD+Exp"] = Dict{String,MathProgBase.AbstractMathProgSolver}()
-# if eco
-#     solvers["SOC"]["ECOS"] = solvers["Exp+SOC"]["ECOS"] = ECOS.ECOSSolver(verbose=false)
-# end
+if eco
+    solvers["SOC"]["ECOS"] = solvers["Exp+SOC"]["ECOS"] = ECOS.ECOSSolver(verbose=false)
+end
 if scs
     solvers["Exp+SOC"]["SCS"] = SCS.SCSSolver(eps=1e-5, max_iters=1000000, verbose=0)
     solvers["SOC"]["SCS"] = solvers["PSD+SOC"]["SCS"] = solvers["PSD+Exp"]["SCS"] = SCS.SCSSolver(eps=1e-6, max_iters=1000000, verbose=0)
