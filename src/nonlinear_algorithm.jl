e5a4c3e61d935d2c6a39ffbfa19e253dff1bf985
Chris C <chriscoey@users.noreply.github.com>
2017-06-11 21:10:55 -0400

    remove todo

--- src/nonlinear_algorithm.jl
+++ src/nonlinear_algorithm.jl
@@ -4,11 +4,6 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#=========================================================
-TODO
-Rewrite to the standard of conicmodel.jl
-=========================================================#
-
 using JuMP
 
 type PajaritoNonlinearModel <: MathProgBase.AbstractNonlinearModel
